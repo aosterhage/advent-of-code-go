fbb291b443109b184ee79c904c9c7af52122d216
Aaron <aaron@aaron-macbook.maine.rr.com>
2023-11-02 22:22:54 -0400

    Fix variable name in filter.go
    Fixes one of the variable names in the Filter() function to be "t"
    because it represents a single T object from the []T "ts".

--- util/filter.go
+++ util/filter.go
@@ -1,9 +1,9 @@
 package util
 
 func Filter[T any](ts []T, test func(T) bool) (ret []T) {
-	for _, s := range ts {
-		if test(s) {
-			ret = append(ret, s)
+	for _, t := range ts {
+		if test(t) {
+			ret = append(ret, t)
 		}
 	}
 	return
